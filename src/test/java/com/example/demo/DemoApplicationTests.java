32bc78420119cfe92431870f2c129bdb16836ef5
Thorodin89 <47855597+Thorodin89@users.noreply.github.com>
2022-11-19 12:47:09 -0300

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,34 @@
-package com.example.demo;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crmpsico;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Scanner;
 
-@SpringBootTest
-class DemoApplicationTests {
+/**
+ *
+ * @author luanv
+ */
+public class Crmpsico {
+    
+    
+    public static String scanner(){
+        Scanner scan = new Scanner(System.in);
+        String digite = scan.next();
+        
+        return digite;
+    }
 
-	@Test
-	void contextLoads() {
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bem vindo ao CRM");
+        System.out.println("Digite seu nome: ");
+        scanner();
+         
+    }
+    
 }
